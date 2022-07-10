cc71671e2bde9aa0aab4e72538f6ecc8408dda12
ThunderWoman <yuliiasigva@gmail.com>
2022-07-10 11:00:21 +0300

    HW_4 mongoose / middlewares

--- app.js
+++ app.js
@@ -1,52 +1,29 @@
-const fs = require('fs/promises');
-const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
 
-const sortFolder = async (read, gender, write) => {
-    try {
-        const files = await fs.readdir(path.join(__dirname, read));
+const { userRouter } = require('./routes');
+const { configs } = require('./configs');
 
-        for (const file of files) {
-            const redFolderPath = path.join(__dirname, read, file);
-            const data = await fs.readFile(redFolderPath);
-            const user = JSON.parse(data.toString());
+mongoose.connect(configs.MONGO_URL);
 
-            if (user.gender === gender) {
-                await fs.rename(redFolderPath, path.join(__dirname, write, file));
-            }
-        }
-    }catch (e) {
-        console.error(e);
-    }
-}
+const app = express();
+app.use(express.json());
 
-sortFolder('girls', 'male', 'boys');
-sortFolder('boys', 'female', 'girls');
+app.use('/users', userRouter);
 
+app.use('*', (req, res) => {
+    res.status(404).json('Route not found');
+});
 
+app.use((err, req, res, next) => {
+    res
+        .status(err.status || 500)
+        .json({
+            error: err.message || 'Unknown Error',
+            code: err.status || 500
+        });
+});
 
-// const fs = require('fs');
-// const path = require('path');
-//
-// const sortFolder = (read, gender, write) => {
-//   fs.readdir(path.join(__dirname, read), (err, files) => {
-//     if (err) return console.log(err);
-//
-//     files.forEach((file) => {
-//       const redFolderPath = path.join(__dirname, read, file)
-//       fs.readFile(redFolderPath, (err, data) => {
-//         if (err) return console.log(err);
-//
-//         const user = JSON.parse(data.toString());
-//
-//         if(user.gender === gender) {
-//           fs.rename(redFolderPath, path.join(__dirname, write, file), (err) => {
-//             if (err) return console.log(err);
-//           });
-//         }
-//       });
-//     });
-//   });
-// }
-//
-// sortFolder('girls', 'male', 'boys');
-// sortFolder('boys', 'female', 'girls');
+    app.listen(configs.PORT, () => {
+        console.log(`Started on port ${configs.PORT}`);
+    });
