59447865e47f38b5498aabd7674289b1df29a0a5
Yuliia Sigva <90891512+ThunderWoman@users.noreply.github.com>
2022-07-13 18:45:00 +0300

    HW_8 SMS (twilio)

--- services/index.js
+++ services/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     emailService: require('./email.service'),
+    smsService: require('./sms.service'),
     fileService: require('./user.service'),
     passwordService: require('./password.service'),
 }
